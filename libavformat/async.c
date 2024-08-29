e4360c180ffe0f293e51ae3c05e40c737011c94f
shiki <pitatensai@gmail.com>
2024-08-29 11:18:18 +0800

    Revert "更新到6.1.1版本"
    This reverts commit d5ec2408f2983c80262b245538c907ef2aaf9961.

--- libavformat/async.c
+++ libavformat/async.c
@@ -489,7 +489,7 @@ static const AVClass async_context_class = {
     .version    = LIBAVUTIL_VERSION_INT,
 };
 
-URLProtocol ff_async_protocol = {
+const URLProtocol ff_async_protocol = {
     .name                = "async",
     .url_open2           = async_open,
     .url_read            = async_read,
